3b974ed4346fb9ee7eaaeeaf0cf9385771bc423b
Rabi Alaya <rabi-alaya@uiowa.edu>
2024-12-07 18:09:43 -0600

    added projects

--- swiper.js
+++ swiper.js
@@ -5,13 +5,10 @@ const swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
     },
     slidesPerView: 1, // Show one slide at a time
-    spaceBetween: 30, // Add space between slides
+    spaceBetween: 80, // Add space between slides
     breakpoints: {
     768: {
         slidesPerView: 2, // Show 2 slides on tablets
-    },
-    1024: {
-        slidesPerView: 3, // Show 3 slides on larger screens
     },
     },
 });
