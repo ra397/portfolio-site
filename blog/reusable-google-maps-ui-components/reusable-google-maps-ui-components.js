e3ade1477985c855610a7c66001532e9b6b70932
Alaya <ralaya@uiowa.edu>
2026-02-25 18:42:25 -0600

    starting first blog

---
+++ blog/reusable-google-maps-ui-components/reusable-google-maps-ui-components.js
@@ -0,0 +1,8 @@
+const customOverlayMap = new google.maps.Map(document.getElementById("customOverlayMap"), {
+    center: {lat: 42.1174, lng: -93.1366},
+    zoom: 6,
+    minZoom: 4,
+    maxZoom: 12,
+    clickableIcons: false,
+    disableDefaultUI: true,
+});
